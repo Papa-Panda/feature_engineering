c034f2f0cb4f1cb690d74afb310f91cc7c63b43c
Jun Gao <0618johnny@gmail.com>
2021-12-24 11:23:15 -0800

    Create summary.py

---
+++ summary.py
@@ -0,0 +1,46 @@
+# 特征工程到底是什么？ - SpongeBob的回答 - 知乎 https://www.zhihu.com/question/29316149/answer/607394337
+# # Feature engineering statistics
+# # https://www.slideshare.net/HJvanVeen/feature-engineering-72376750
+
+# 1.类别特征
+# one-hot encoding
+# hash encoding
+# label encoding
+# count encoding
+# label-count encoding
+# target encoding
+# category embedding
+# Nan encoding
+# polynomial encoding
+# expansion encoding
+# consolidation encoding
+
+# 2.数值特征
+# rounding
+# binning
+# scaling
+# imputation
+# interactions
+# no linear encoding
+# row statistics
+
+# 4.时间特征
+
+# 5.空间特征
+
+# 6.自然语言处理
+
+# 7.深度学习/NN
+
+# 8.Leakage
+
+# 9. 当然以上并不全，还有很多非常经典的聚合统计，Graph上的特征工程等等。
+
+# 10. 文本相似性是一个非常好的体味特征工程威力，对比深度学习效果的一个问题，不妨去看看。
+
+
+
+# 作者：SpongeBob
+# 链接：https://www.zhihu.com/question/29316149/answer/607394337
+# 来源：知乎
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
