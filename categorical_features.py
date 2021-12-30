f321945cb94faa8fbf99b5572136fa2de1b6ac96
Jun Gao <0618johnny@gmail.com>
2021-12-29 19:08:50 -0800

    Update categorical_features.py

--- categorical_features.py
+++ categorical_features.py
@@ -1,1 +1,52 @@
+"""
+This module contains common python implementaions of applying categorical features for Machine Learning. 
+Techniques are summarized in https://www.slideshare.net/HJvanVeen/feature-engineering-72376750. 
 
+:class:`categoricalFeatureEngineering` is the fundamental class for turning categorical features into numerical features. 
+"""
+
+class categoricalFeatureEngineering
+    """categoricalFeatureEngineering .
+
+    Attributes:
+
+        data: input data.
+    """
+    def __init__(
+        self,
+        start_time: pd.DataFrame,
+    ) -> None:
+      pass
+    
+    def oneHotEncoding():
+      pass
+    
+    def hashEncoding():
+      pass
+    
+    def labelEncoding():
+      pass
+    
+    def countEncoding():
+      pass
+    
+    def labelCountEncoding():
+      pass
+    
+    def targetEncoding():
+      pass
+    
+    def categoryEmbedding():
+      pass
+    
+    def nanEncoding():
+      pass
+    
+    def polynomialEncoding():
+      pass
+    
+    def expansionEncoding():
+      pass
+
+    def consolidationEncoding():
+      pass
